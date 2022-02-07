e297c954d65240dff472b66746a351efc2e3bbd1
Angel Rivas <31136883+uepatickets@users.noreply.github.com>
2022-02-07 15:42:40 -0400

    update sw.js airhorner

--- sw.js
+++ sw.js
@@ -1,30 +1,48 @@
-/**
- * Welcome to your Workbox-powered service worker!
+/*
  *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
+ *  Evento
  *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
  */
 
- importScripts("https://uepatickets.github.io/worldhottesttour.uepa.com/js/bootstrap.bundle.min.js");
- 
- workbox.clientsClaim();
- 
- /**
-  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
-  * requests for URLs in the manifest.
-  * See https://goo.gl/S9QRab
-  */
- self.__precacheManifest = [].concat(self.__precacheManifest || []);
- workbox.precaching.suppressWarnings();
- workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
- 
- workbox.routing.registerNavigationRoute("/worldhottesttour.uepa.com/index.html", {
-   
-   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
- });
+const version = "0.6.20";
+const cacheName = `evento-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `index.html`,
+        `queue.html`,
+        `js/bootstrap.bundle.min.js`,
+        `css/bootstrap.min.css`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
